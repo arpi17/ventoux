b662219d5b48e1714395e2ac0a20f861409d2ae9
Arpad Illyes <arpadillyes93@gmail.com>
2022-06-14 23:03:57 +0200

    Update readme and remove comments

--- README.md
+++ README.md
@@ -10,3 +10,9 @@ Vue 3 starter template
 - [Vitest](https://vitest.dev/) with [happy-dom](https://github.com/capricorn86/happy-dom)
 - [vue-test-utils](https://test-utils.vuejs.org/)
 - [Histoire](https://histoire.dev/)
+- [UnoCSS](https://github.com/unocss/unocss#readme) with
+  - [Default preset](https://github.com/unocss/unocss/tree/main/packages/preset-uno)
+  - [Attributify mode](https://github.com/unocss/unocss/tree/main/packages/preset-attributify)
+  - [CSS Directives](https://github.com/unocss/unocss/tree/main/packages/transformer-directives)
+  - [Variant Groups](https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group)
+- [file-system-routing](https://github.com/hannoeru/vite-plugin-pages)
--- unocss.config.ts
+++ unocss.config.ts
@@ -7,16 +7,9 @@ import {
 } from 'unocss';
 
 export default defineConfig({
-  // https://github.com/unocss/unocss#shortcuts
   shortcuts: {
     btn: 'py-2 px-4 border-none rounded-lg bg-green text-white hover:(bg-green-500 cursor-pointer) rounded-full text-sm font-medium',
   },
-  presets: [
-    presetUno(), // https://github.com/unocss/unocss/tree/main/packages/preset-uno#usages
-    presetAttributify(), // https://github.com/unocss/unocss/tree/main/packages/preset-attributify#attributify-mode
-  ],
-  transformers: [
-    transformerDirectives(), // https://github.com/unocss/unocss/tree/main/packages/transformer-directives#usage
-    transformerVariantGroup(), // https://github.com/unocss/unocss/tree/main/packages/transformer-variant-group#usage
-  ],
+  presets: [presetUno(), presetAttributify()],
+  transformers: [transformerDirectives(), transformerVariantGroup()],
 });
