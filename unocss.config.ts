4c20e4eea10598d3d72b450645df5270cd60b9fb
Arpad Illyes <arpadillyes93@gmail.com>
2022-06-20 10:12:47 +0200

    Add link to UnoCSS docs

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,6 +6,7 @@ import {
   transformerVariantGroup,
 } from 'unocss';
 
+// https://github.com/unocss/unocss#configurations
 export default defineConfig({
   shortcuts: {
     btn: 'py-2 px-4 border-none rounded-lg bg-green text-white hover:(bg-green-500 cursor-pointer) rounded-full text-sm font-medium',
