a8c2cd1c95e27fbd0ab298cfc72a032ee21ae9da
Arpad Illyes <arpadillyes93@gmail.com>
2022-06-23 17:20:41 +0200

    Reset styles

--- src/App.vue
+++ src/App.vue
@@ -7,10 +7,13 @@
 </template>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
