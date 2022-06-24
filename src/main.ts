8574a17838b0604f94f1a9626be30f18955ef867
Arpad Illyes <arpadillyes93@gmail.com>
2022-06-24 16:46:07 +0200

    Add comments for plugins and app

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,17 @@ import App from './App.vue';
 import routes from '~pages';
 import 'uno.css';
 
+// Router
+// https://router.vuejs.org/guide/#javascript
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
+// Head manager
+// https://github.com/vueuse/head#usage
 const head = createHead();
 
+// Creating the Vue Application
+// https://vuejs.org/guide/essentials/application.html#creating-a-vue-application
 createApp(App).use(router).use(head).mount('#app');
