59d7fab1d8368f99a367ff474cd26dec693a776a
Arpad Illyes <arpadillyes93@gmail.com>
2022-06-29 14:04:30 +0200

    Add environment variable for port

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,10 +1,7 @@
 import { PlaywrightTestConfig } from '@playwright/test';
-import viteConfig from './vite.config';
 
-// FIXME: config can be async
-// see https://main.vitejs.dev/config/#async-config
-const PORT = viteConfig.server.port || 3000;
-const baseURL = `http://localhost:${PORT}`;
+const port = process.env.VITE_PORT || 5173;
+const baseURL = `http://localhost:${port}`;
 
 const config: PlaywrightTestConfig = {
   webServer: {
--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,30 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
+import { configDefaults } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 import Components from 'unplugin-vue-components/vite';
 import Pages from 'vite-plugin-pages';
 import Unocss from 'unocss/vite';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 5173,
-  },
-  plugins: [vue(), Components(), Pages(), Unocss()],
+export default defineConfig(({ mode }) => {
+  // Only loads env vars prefixed with `VITE_`
+  const env = loadEnv(mode, process.cwd());
+  const port = Number(env.VITE_PORT) || 5173;
 
-  // Vitest config
-  // @see https://vitest.dev/guide/#configuring-vitest
-  // FIXME: types don't work with Vite 3
-  test: {
-    environment: 'happy-dom',
-  },
+  return {
+    server: {
+      port,
+    },
+    plugins: [vue(), Components(), Pages(), Unocss()],
+
+    // Vitest config
+    // @see https://vitest.dev/guide/#configuring-vitest
+    // FIXME: the types are not correct
+    test: {
+      environment: 'happy-dom',
+      exclude: [...configDefaults.exclude, 'e2e/*'],
+    },
+  };
 });
